8253d54230dc96c4e0ec261823303db3f2f4dc9b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-13 19:40:35 -0500

    Move bg img to header and add some styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import background from '../../stars.png';
 import './Header.css';
 
 export default function Header() {
   return (
-    <div>
+    <div style={{ backgroundImage: `url(${background})` }}>
       <h1>Zodiac Signs</h1>
     </div>
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { zodiac } from '../../data.js';
 import ZodiacCard from '../ZodiacCard/ZodiacCard.js';
-import background from '../../stars.png';
 import './Main.css';
 
 export default function Main() {
   return (
-    <main style={{ backgroundImage: `url(${background})` }}>
+    <main>
       {zodiac.map(({ id, name, dates, symbol }) => (
         <ZodiacCard
           key={id}
